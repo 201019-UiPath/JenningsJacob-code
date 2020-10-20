254ce2f035ad452f6054dbc71f365c62db342235
Jacob <jacobjennings510@gmail.com>
2020-10-20 12:32:04 -0700

    Added Hero class

---
+++ 1-CSharp/HerosApp/HerosLib/Hero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HerosLib
+{
+    public class Hero
+    {
+        // int id; // value type => System.Int32
+        // public int Id{
+        //     get{
+        //         return id;
+        //     }
+        //     set{
+        //         id = value;
+        //     }
+        // }
+        public int Id{get;set;}
+        string name; // reference type => System.String
+        // Constructor
+        public Hero() {
+            Id = 1;
+            name = "Dr. Strange";
+        }
+        // snippet: ctor + <tab>
+        public Hero(int id, string name)
+        {
+            this.Id = id;
+            this.name = name;
+        }
+
+    }
+}
