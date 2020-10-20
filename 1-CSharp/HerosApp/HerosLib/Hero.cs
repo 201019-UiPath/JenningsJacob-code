8de381d75392510ccb8fe03a7dd4f9462a5fcf16
Jacob <jacobjennings510@gmail.com>
2020-10-20 13:44:46 -0700

    added jagged arrays

--- 1-CSharp/HerosApp/HerosLib/Hero.cs
+++ 1-CSharp/HerosApp/HerosLib/Hero.cs
@@ -2,30 +2,40 @@
 
 namespace HerosLib
 {
-    public class Hero
-    {
-        // int id; // value type => System.Int32
-        // public int Id{
-        //     get{
-        //         return id;
-        //     }
-        //     set{
-        //         id = value;
-        //     }
-        // }
-        public int Id{get;set;}
-        string name; // reference type => System.String
-        // Constructor
-        public Hero() {
-            Id = 1;
-            name = "Dr. Strange";
-        }
-        // snippet: ctor + <tab>
-        public Hero(int id, string name)
-        {
-            this.Id = id;
-            this.name = name;
-        }
-
+    #region old way of creating class members
+    // public class Hero
+    // {
+    //     // int id; // value type => System.Int32
+    //     // public int Id{
+    //     //     get{
+    //     //         return id;
+    //     //     }
+    //     //     set{
+    //     //         id = value;
+    //     //     }
+    //     // }
+    //     public int Id{get;set;} // automatic properties
+    //     string name; // reference type => System.String
+    //     // Constructor
+    //     public Hero() {
+    //         Id = 1;
+    //         name = "Dr. Strange";
+    //     }
+    //     // snippet: ctor + <tab>
+    //     public Hero(int id, string name)
+    //     {
+    //         this.Id = id;
+    //         this.name = name;
+    //     }
+    // }
+    #endregion
+    #region modern way of creating class members
+    public class Hero {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string[] superPowers = new string[10];
+        // jagged array
+        public int[][] ja = new int[3][];
     }
+    #endregion
 }
--- 1-CSharp/HerosApp/HerosUI/Program.cs
+++ 1-CSharp/HerosApp/HerosUI/Program.cs
@@ -8,17 +8,45 @@ namespace HerosUI
         static void Main(string[] args)
         {
             #region default constructor
-            Hero obj = new Hero();
+            // Hero obj = new Hero();
             // Console.WriteLine($"{obj.id}\t{obj.name}");
             #endregion
             #region parameterized constructor
-            Hero obj1 = new Hero(101, "Hulk");
+            // Hero obj1 = new Hero(101, "Hulk");
             // Console.WriteLine($"{obj1.id}\t{obj1.name}"); // Cannot access variables if not public
             #endregion
             #region access via properties
-            Console.WriteLine(obj1.Id);                 // Read property value
-            obj1.Id=202;
-            Console.WriteLine($"New ID = {obj1.Id}");   // Write into property
+            // Console.WriteLine(obj1.Id);                 // Read property value
+            // obj1.Id=202;
+            // Console.WriteLine($"New ID = {obj1.Id}");   // Write into property
+            #endregion
+            #region accessing 1D arrays
+            Hero obj = new Hero();
+            // Console.Write("Please enter your hero's id: ");
+            // obj.Id = Int32.Parse(Console.ReadLine());
+            // Console.Write("Please enter your hero's name: ");
+            // obj.Name = Console.ReadLine();
+            // Console.Write("Please enter your superpower: ");
+            // obj.superPowers[0] = Console.ReadLine();
+            // Console.Write($"{obj.Id} {obj.Name} {obj.superPowers[0]}");
+
+            // Jagged arrays
+            obj.ja[0] = new int[2]; // first column
+            obj.ja[1] = new int[3]; // second column
+            obj.ja[2] = new int[1]; // third column
+
+            obj.ja[0][0] = 10;
+            obj.ja[1][2] = 20;
+            // Console.WriteLine(obj.ja.Length);
+
+            foreach (var row in obj.ja)
+            {
+                for(int i = 0; i < row.Length; i++)
+                    Console.Write($"{row[i]}\t");
+                Console.WriteLine();
+            }
+
+
             #endregion
         }
     }
