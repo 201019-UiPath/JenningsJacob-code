ebea5ca1eaa2556e39a7638b45a4a5c836071487
Jacob <jacobjennings510@gmail.com>
2020-10-20 12:32:47 -0700

    testing hero class

--- 1-CSharp/HerosApp/HerosUI/Program.cs
+++ 1-CSharp/HerosApp/HerosUI/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using HerosLib;
 
 namespace HerosUI
 {
-    class Program
+    class Program:Hero
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region default constructor
+            Hero obj = new Hero();
+            // Console.WriteLine($"{obj.id}\t{obj.name}");
+            #endregion
+            #region parameterized constructor
+            Hero obj1 = new Hero(101, "Hulk");
+            // Console.WriteLine($"{obj1.id}\t{obj1.name}"); // Cannot access variables if not public
+            #endregion
+            #region access via properties
+            Console.WriteLine(obj1.Id);                 // Read property value
+            obj1.Id=202;
+            Console.WriteLine($"New ID = {obj1.Id}");   // Write into property
+            #endregion
         }
     }
 }
