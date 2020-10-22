4c9f1413c5a88a944d387546a1a4f4b35ad4b8cc
Jacob <jacobjennings510@gmail.com>
2020-10-22 13:35:36 -0700

    Added business logic layer

---
+++ 1-CSharp/HerosApp/HerosBL/HeroBL.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HerosLib;
+using HerosDB;
+
+
+namespace HerosBL
+{
+    public class HeroBL
+    {
+        IRepository repo = new FileRepo();
+        public void AddHero(Hero newHero){
+            // You can add the business logic to check stuff
+            // Ex: check if hero name is unique
+            repo.AddHeroAsync(newHero);
+        }
+
+        public List<Hero> GetAllHeros() {
+            Task<List<Hero>> getHeros = repo.GetAllHerosAsync();
+            return getHeros.Result;
+        }
+    }
+}
