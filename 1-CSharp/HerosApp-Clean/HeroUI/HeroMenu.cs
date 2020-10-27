dc6960e11ff132a4b1a3c795c9667ff8e10bb97f
Jacob <jacobjennings510@gmail.com>
2020-10-27 09:27:27 -0700

    prints out list of heroes

--- 1-CSharp/HerosApp-Clean/HeroUI/HeroMenu.cs
+++ 1-CSharp/HerosApp-Clean/HeroUI/HeroMenu.cs
@@ -44,7 +44,7 @@ namespace HeroUI
                         heroService.GetAllHeroes();
                         foreach(var hero in heroService.GetAllHeroes())
                         {
-                            Console.WriteLine($"Name: {hero.Alias}");
+                            Console.WriteLine($"Hero: \n\tHero Alias: {hero.Alias} \n\tHero Real Name: {hero.RealName} \n\tHero Hideout: {hero.HideOut}");
                         }
                         break;
                     case "2":
