7ec1b0fa213c0a48286ffb113eb13b2e181c43c4
Jacob <jacobjennings510@gmail.com>
2020-11-03 12:48:33 -0800

    updated notes

---
+++ 3-ASP.NET Core/HerosApp/HerosWeb/Controllers/SuperHeroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+
+namespace HerosWeb.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        public String Index()
+        {
+            return "Welcome to Super Hero World";
+        }
+    }
+}
